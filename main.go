3e2958cf2898c67da13245b49e790e4f18a5c788
PushpaKumara89 <kpushpakumarajd@gmail.com>
2022-12-13 23:28:56 +0530

    connect mysql database implement functions -create/add user                     -delete                     -get/one user                     -getAll pending impl update user details

--- main.go
+++ main.go
@@ -1,80 +1,111 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"strconv"
 )
 
-type User struct{
-	Id			int64  `json:"id"`
-	FirstName	string `json:"first_name"`
-	LastName	string `json:"last_name"`
-	Email		string `json:"email"`
+type User struct {
+	Id        int64  `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
 }
 
 /*Local data*/
-var users = [] User {
-	{1,"Anil", "Kumble", "Anl@gm.com"},
-	{2,"john", "smith", "john@gm.com"},
-	{3,"Adem", "gill kst", "kst@gm.com"},
+var users = []User{
+	{1, "Anil", "Kumble", "Anl@gm.com"},
+	{2, "john", "smith", "john@gm.com"},
+	{3, "Adem", "gill kst", "kst@gm.com"},
 }
-
-
 
 func main() {
 	initializeGinRouter()
 }
+
 /*----------------gorilla-----------------*/
 // Gin function
 
 func RunInfo(context *gin.Context) {
-	context.JSON(http.StatusOK,gin.H{
+	context.JSON(http.StatusOK, gin.H{
 		"message": "hello server running 8000 port",
-		"status": true,
+		"status":  true,
 	})
 }
 
 func GetUsers(context *gin.Context) {
+	db := DBConnection()
+	// Execute the query
+	results, err := db.Query("SELECT * FROM user")
+	db.Close()
+	if err != nil {
+		log.Fatal(err.Error()) // proper error handling instead of log in your app
+	}
+	var users []User
+	for results.Next() {
+		var user User
+		// for each row, scan the result into our tag composite object
+		err = results.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email)
+		if err != nil {
+			log.Print(err.Error()) // proper error handling instead of log in your app
+		}
+		// and then print out the tag's Name attribute
+		users = append(users, user)
+	}
 	context.IndentedJSON(http.StatusOK, users)
 }
 
 func CreateUser(context *gin.Context) {
 	var newUser User
-
+	db := DBConnection()
 	err := context.BindJSON(&newUser)
 	if err != nil {
+		db.Close()
 		context.String(http.StatusBadRequest, err.Error())
 		return
 	}
 
-	newUser.Id = int64(len(users)+1)
-	users = append(users, newUser)
+	insert, errIn := db.Query("INSERT INTO user VALUES (?, ?, ?, ?)", nil, newUser.FirstName, newUser.LastName, newUser.Email)
+	insert.Close()
+	db.Close()
+
+	if errIn != nil {
+		context.String(http.StatusForbidden, err.Error())
+		return
+	}
 	context.IndentedJSON(http.StatusCreated, newUser)
 }
 
-
 func GetUser(context *gin.Context) {
-	id , err := strconv.Atoi(context.Param("id"))
+	db := DBConnection()
+	id, err := strconv.Atoi(context.Param("id"))
 	//check if error occured
-	if err != nil{
+	if err != nil {
 		//executes if there is any error
-		println(err)
-		context.IndentedJSON(http.StatusBadRequest,"only accept numbers..")
+		db.Close()
+		log.Print(err.Error())
+		context.IndentedJSON(http.StatusBadRequest, "only accept numbers..")
 		return
-
 	}
-	for i:=range users{
-		println(users[i].Id== int64(id))
-		if int64(id) == users[i].Id {
-			context.IndentedJSON(http.StatusFound, users[i])
-			return
-		}
+	var user User
+	// Execute the query
+	err = db.QueryRow("SELECT * FROM user where id = ?", id).Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email)
+	db.Close()
+	if err != nil {
+		log.Print(err.Error())
+		context.String(http.StatusForbidden, err.Error()) // proper error handling instead of panic in your app
+		return
+	} else {
+		context.IndentedJSON(http.StatusOK, user)
+		return
 	}
-	context.IndentedJSON(http.StatusNotFound,"Invalid id check again...")
+	log.Print("data Not Found")
+	context.IndentedJSON(http.StatusNotFound, "Invalid id check again...")
 }
-
 
 func Update(context *gin.Context) {
 	var updatedUser User
@@ -83,10 +114,10 @@ func Update(context *gin.Context) {
 	if err != nil {
 		context.String(http.StatusBadRequest, err.Error())
 		return
-	}else {
-		for i:=range users{
+	} else {
+		for i := range users {
 			if updatedUser.Id == users[i].Id {
-				users[i] =updatedUser
+				users[i] = updatedUser
 				context.IndentedJSON(http.StatusOK, updatedUser)
 				return
 			}
@@ -96,40 +127,29 @@ func Update(context *gin.Context) {
 }
 
 func Delete(context *gin.Context) {
-	id , err := strconv.Atoi(context.Param("id"))
+	db := DBConnection()
+	id, err := strconv.Atoi(context.Param("id"))
 	//check if error occured
-	if err != nil{
+	if err != nil {
 		//executes if there is any error
 		println(err)
-		context.IndentedJSON(http.StatusBadRequest,"only accept numbers..")
+		context.IndentedJSON(http.StatusBadRequest, "only accept numbers..")
 		return
 	}
 
-	if len(users) >0{
-		for i:=range users{
-			if int64(id) == users[i].Id {
-				users = RemoveIndex(1+i)
-				context.IndentedJSON(http.StatusOK, users)
-				return
-			}
-		}
-	}
-	context.IndentedJSON(http.StatusNotFound, "User Notfound :")
-}
-
-
-func RemoveIndex(index int) []User {
-	return append(users[:index-1], users[index:]...)
+	db.Exec("DELETE FROM user WHERE id= ?", id)
+	db.Close()
+	context.IndentedJSON(http.StatusOK, "Success")
 }
 
 //Gin
 
-func initializeGinRouter()  {
+func initializeGinRouter() {
 
 	r := gin.Default()
 	r.GET("/", RunInfo)
 
-	userRoute:= r.Group("/users")
+	userRoute := r.Group("/users")
 	{
 		userRoute.GET("/getAll", GetUsers)
 		userRoute.POST("/", CreateUser)
@@ -138,16 +158,26 @@ func initializeGinRouter()  {
 		userRoute.DELETE("/delete/:id", Delete)
 	}
 
-
-
-	err:= r.Run("localhost:8000")
+	err := r.Run("localhost:8000")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+func DBConnection() *sql.DB {
+	// Open up our database connection.
+	db, err := sql.Open("mysql", "root:1234@tcp(localhost:3307)/test")
 
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		println("Connection false")
+		log.Print(err.Error())
+	} else {
+		println("Connected")
+	}
+	return db
+}
 
 /*----------------gorilla-----------------*/
 // function for gorilla mux
@@ -166,12 +196,12 @@ func DeleteUser(writer http.ResponseWriter, request *http.Request) {
 
 
 func GetUser(writer http.ResponseWriter, request *http.Request) {
-	
+
 }
 
 
 func CreateUser(writer http.ResponseWriter, request *http.Request) {
-	
+
 }
 
 func GetUsers(writer http.ResponseWriter, request *http.Request) {
